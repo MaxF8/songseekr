2fcf9677edcb6917e14dbf171dde368687223cf2
MaxF8 <64173856+MaxF8@users.noreply.github.com>
2022-09-03 23:58:18 -0400

    fix router bug

--- client/src/pages/LikedSongData/LikedSongData.js
+++ client/src/pages/LikedSongData/LikedSongData.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import { BrowserRouter as Link } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 
 import { makeStyles } from "@material-ui/core/styles";
 
